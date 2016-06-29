6b92915313db9029d449a140b291943c043052d8
Patrick Best <patrickbest@patrickbest.com>
2016-06-29 15:57:29 +0100

    set default size to 50px
    `$field->args( 'preview_size' )` is always 50px [set somewhere i cant
    find] so never sets to the default of 150px. Better to set the default
    to 50px and always bother to set thumbnail size

--- js/script.js
+++ js/script.js
@@ -7,22 +7,22 @@ jQuery( document ).ready(function($) {
 			wp.media.gallery.edit(gallerysc).on('update', function(g) {
 				var id_array = [];
 				var fileGroup = [];
-				
-				$( ".cmb2-media-status li" ).remove();
-				
-				$.each(g.models, function(id, img) { 
-				
-					id_array.push(this.id); 
-					var width = this.attributes.sizes.thumbnail.width ? this.attributes.sizes.thumbnail.width  : 150;
-					var height = this.attributes.sizes.thumbnail.height ? this.attributes.sizes.thumbnail.height  : 150;
 
-					uploadStatus = '<li class="img-status"><img width="'+width+'" height="'+height+'" src="' + this.attributes.sizes.thumbnail.url+ '" class="attachment-150x150" alt=""></li>';
+				$( ".cmb2-media-status li" ).remove();
+
+				$.each(g.models, function(id, img) {
+
+					id_array.push(this.id);
+					var width = this.attributes.sizes.thumbnail.width ? this.attributes.sizes.thumbnail.width  : 50;
+					var height = this.attributes.sizes.thumbnail.height ? this.attributes.sizes.thumbnail.height  : 50;
+
+					uploadStatus = '<li class="img-status"><img width="'+width+'" height="'+height+'" src="' + this.attributes.sizes.thumbnail.url+ '" class="attachment-50x50" alt=""></li>';
 					fileGroup.push( uploadStatus );
-				
+
 				});
-				
+
 					$('input[type=hidden]', instance).val(id_array.join(","));
-				
+
 				$( fileGroup ).each( function() {
 
 					$('.cmb2-media-status').slideDown().append(this);
